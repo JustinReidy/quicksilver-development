fa2568ca794f49bddf28f88ff4a44749d43cbda0
Justin Reidy <Justintreidy@gmail.com>
2023-04-21 11:59:21 -0600

    landing page

---
+++ app/(default)/page.tsx
@@ -0,0 +1,22 @@
+import Hero from '@/components/hero'
+import Features from '@/components/features'
+import FeaturesBlocks from '@/components/features-blocks'
+import Testimonials from '@/components/testimonials'
+import Newsletter from '@/components/newsletter'
+
+export const metadata = {
+  title: 'Home',
+  description: 'Page Description'
+}
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <Features />
+      <FeaturesBlocks />
+      <Testimonials />
+      <Newsletter />
+    </>
+  )
+}
